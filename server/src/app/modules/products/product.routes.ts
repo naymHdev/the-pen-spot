951b843b81358384e1b7a62dc101dc8aa8370e87
MacBook Pro <naym100m@gmail.com>
2025-01-27 12:49:14 +0600

    [install] compleate tailwind css by vite and install tailwind css

--- server/src/app/modules/products/product.routes.ts
+++ server/src/app/modules/products/product.routes.ts
@@ -2,16 +2,19 @@ import express from 'express';
 import { ProductController } from './product.controller';
 import { ProductValidations } from './product.validation';
 import { validateRequest } from '../../middlewares/validateRequest';
+import { auth } from '../../middlewares/auth';
 const router = express.Router();
 
 router.post(
   '/create-stationary-product',
+  auth('admin'),
   validateRequest(ProductValidations.stationeryValidationsProductSchema),
   ProductController.createStationaryProduct,
 );
 
 router.get(
   '/get-all-stationary-products',
+  auth('admin'),
   ProductController.getALlStationaryProduct,
 );
 export const ProductRoutes = router;
