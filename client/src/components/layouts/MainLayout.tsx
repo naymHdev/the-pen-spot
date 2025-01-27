b080140f56626627010e39ff4cdd91f3121d83fc
MacBook Pro <naym100m@gmail.com>
2025-01-27 23:04:22 +0600

    [create ] a header menubar

--- client/src/components/layouts/MainLayout.tsx
+++ client/src/components/layouts/MainLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../shared/Navbar";
+import Menubar from "../shared/Menubar";
 
 const MainLayout = () => {
   return (
     <>
       <Navbar />
+      <Menubar />
       <Outlet />
     </>
   );
---
+++ client/src/components/shared/Menubar.tsx
@@ -0,0 +1,44 @@
+import { List, Sparkles } from "lucide-react";
+
+const Menubar = () => {
+  return (
+    <>
+      <div>
+        <div className=" container mx-auto py-5">
+          <div className="hidden lg:grid grid-cols-5 items-center">
+            <div className=" col-span-1">
+              <div className=" flex items-center gap-3">
+                <List />
+                <h3 className=" text-primary-text text-md font-semibold">
+                  Browse Categories
+                </h3>
+              </div>
+            </div>
+            <div className=" col-span-3">
+              <div>
+                <ul className=" flex items-center gap-8 text-primary-text font-medium">
+                  <li>Home</li>
+                  <li>Books</li>
+                  <li>Trending</li>
+                  <li>Best Sell</li>
+                  <li>All Products</li>
+                </ul>
+              </div>
+            </div>
+            <div className=" col-span-1">
+              <div className="text-sm font-medium flex items-center gap-3 justify-end">
+                <Sparkles />
+                <p className=" ">
+                  Summer sale discount
+                  <span className=" text-secondary">50% off.</span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Menubar;
